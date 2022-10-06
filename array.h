ecd7dc36d302ac9e367d6a41e85b62fd3ae6b176
Akashneel Raychaudhuri <94317530+Alph3ga@users.noreply.github.com>
2022-10-06 23:19:07 +0530

    modified it to work for C code

--- array.h
+++ array.h
@@ -1,5 +1,11 @@
 #pragma once
 
-extern "C" int* make_Array(int length);
-extern "C" int get(int* base_pointer, int index);
-extern "C" void change(int* base_pointer, int index, int value);
+#ifdef __cplusplus
+extern "C"{
+#endif
+  int* make_Array(int length);
+  int get(int* base_pointer, int index);
+  void change(int* base_pointer, int index, int value);
+#ifdef __cplusplus
+}
+#endif
